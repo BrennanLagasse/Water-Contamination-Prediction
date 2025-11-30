c24cdb1b979ed44e2fc4a0a07033f597f5ba95d9
supersauce189 <williamlelexing@gmail.com>
2025-11-30 13:06:50 -0800

    Added a Data Cleaning File

---
+++ Data Cleaning/Final Dataset Creation.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("(Cleaned) WQP Full Physical Chemical.csv")
+df2 = pd.read_csv("(Cleaned) WQP + MRDS + gNATSGO.csv")
+truth = pd.read_csv("Ground Truth.csv")
+
+df["ActivityStartDate"] = pd.to_datetime(df["ActivityStartDate"])
+mask = (
+    df.sort_values("ActivityStartDate")
+        .groupby(["ActivityLocation/LatitudeMeasure", "ActivityLocation/LongitudeMeasure", "CharacteristicName"])
+        .tail(1)
+        .index
+)
+
+df = df.loc[mask]
+df2 = df2.loc[mask]
+truth = truth.loc[mask]
+
+print(df["CharacteristicName"].value_counts())
+plt.hist(df["ActivityStartDate"].dt.year, bins=40, edgecolor="black")
+plt.xlabel("Year")
+plt.ylabel("Number of Samples")
+plt.title("Dsitribution of Samples across Time")
+plt.show()
+
+df.to_csv("test.csv", index=False)
+df2.to_csv("test2.csv", index=False)
+truth.to_csv("testTruth.csv", index=False)
